e2f8b486516ef54e7d9b49e4038b445d6372bda1
Loren P <160626485+lore4prez@users.noreply.github.com>
2024-09-03 00:42:42 -0400

    Passed exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(someString, num) {
+    if (num >= 0) {
+        let finalString = "";
+        for (let i = 0; i < num; i++)
+            finalString = finalString + someString;
+        return finalString;
+    }
+    else {
+        return "ERROR";
+    }
+    
 };
 
 // Do not edit below this line
