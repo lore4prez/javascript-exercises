502decf2a484a5c235dc3baa086b524c68e9c056
Loren P <160626485+lore4prez@users.noreply.github.com>
2024-09-03 01:33:35 -0400

    passed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(list, ...moreArgs) {
+    let newArr = [];
+    for (let i = 0; i < list.length; i++) {
+        if (!(moreArgs.includes(list[i]))) {
+            newArr.push(list[i]);
+        }
+    }
+    return newArr;
 };
 
 // Do not edit below this line
