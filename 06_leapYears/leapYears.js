e924cee3a4fc98c8456f4478770444e1a6736bba
Loren P <160626485+lore4prez@users.noreply.github.com>
2024-09-03 01:52:19 -0400

    Passed exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(currentYear) {
+    if (currentYear % 4 != 0) return false;
+    else {
+        if (currentYear % 100 == 0 && currentYear % 400 != 0) return false;
+        return true;
+    }
 };
 
 // Do not edit below this line
