fa2d197e522bf32f268edb384970712fdbffacde
Loren P <160626485+lore4prez@users.noreply.github.com>
2024-09-27 01:36:47 -0400

    Completed 08calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,52 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let len = arr.length;
+  let sum = 0;
+  for (let i = 0; i < len; i++) {
+    sum += arr[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let len = arr.length;
+  let res = 1;
+  for (let i = 0; i < len; i++) {
+    res *= arr[i];
+  }
+  return res;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  let res = 1;
+  if (num2 == 0) return res;
+  if (num2 > 0) {
+    for (let i = 0; i < num2; i++) {
+      res *= num1;
+    }
+    return res;
+  }
+  else {
+    for (let i = 0; i < (-1)*num2; i++) {
+      res *= num1;
+    }
+    return (1.00/res);
+  }
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let res = 1;
+  for (let i = num-1; i > 0; i--) {
+    res += res * i;
+  }
+  return res;
 };
 
 // Do not edit below this line
