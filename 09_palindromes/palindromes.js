3c410904902ded356a6c6a0c62dbb8ec14c566e7
Loren P <160626485+lore4prez@users.noreply.github.com>
2024-09-27 02:06:57 -0400

    Completed 09-palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    str = str.replaceAll(" ", "");
+    str = str.replace(/[^\w\s]/g, "");
+    let arr = str.split("");
+  
+    let tempArr = arr.splice(0, arr.length);
+    let reverseArr = tempArr.reverse();
+    let reverseStr = reverseArr.join("");
+    reverseStr = reverseStr.replaceAll(" ", "");
+    // console.log("String: " + str + "\nReverse: " + reverseStr);
+    if (str == reverseStr) return true;
+    return false;
 };
 
 // Do not edit below this line
