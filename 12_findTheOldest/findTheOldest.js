1ce8938f28577862e83e517dadab9f990db088bc
Loren P <160626485+lore4prez@users.noreply.github.com>
2024-09-27 03:20:03 -0400

    Completed 12-findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    /* 
+    Sort the array from oldest to youngest (b-a)
+    Return the very first element in the array (the oldest) as an object
+    */
 
+    arr = arr.map((item) => {
+        let date = new Date().getFullYear();
+        if (item.hasOwnProperty("yearOfDeath")) {
+            return {
+                name: item.name,
+                age: item.yearOfDeath - item.yearOfBirth
+            }
+        }
+        else {
+            return {
+                name: item.name,
+                age: date - item.yearOfBirth
+            }
+        }
+    })
+
+
+    arr.sort((a,b) => b.age - a.age);
+    return arr[0];
 };
 
 // Do not edit below this line
