62f5f176fa2bce9c9107a5d8c0e69ba0e8dc83d0
Loren P <160626485+lore4prez@users.noreply.github.com>
2024-09-27 02:48:09 -0400

    Completed 11-getTheTitles exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    let tempArr = arr.splice(0,arr.length);
+    let res = tempArr.map((item) => item = item.title);
+    return res;
 };
 
 // Do not edit below this line
